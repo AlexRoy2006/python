e95c67b8542663c967ee9c7dfca35c445a47fd3a
AlexRoy2006 <alexroy2028@cs.sjcetpalai.ac.in>
2024-10-01 15:02:24 +0530

    Add files via upload
    my first project

---
+++ Student_Details.py
@@ -0,0 +1,9 @@
+print("Hello Python")
+name=input("enter the student name:")
+roll_number=int(input("Enter roll number:"))
+roll_number=roll_number+1
+cgpa=float(input("enter the CGPA"))
+cgpa=cgpa*10
+print("Name of the student",name)
+print("Roll Number:",roll_number)
+print("Percentage:",cgpa)
