fe6a8a01f1db8fed11df16398ffdba6efe039f95
AlexRoy2006 <alexroy2028@cs.sjcetpalai.ac.in>
2024-10-05 16:23:01 +0530

    Add files via upload

---
+++ CONCATENATED_STRING.py
@@ -0,0 +1,10 @@
+'''
+AUTHOR:Alex roy
+Date: 05-10-2024
+Python program to get the Concacnated string
+'''
+str1="Hello,World!"
+str2=str1[6:11]
+print(str2)
+str4="Everyone!"
+print(str2+" "+ str4)
