202939f65ac0a8d0e85a07556bc0978ecc10ec34
AlexRoy2006 <alexroy2028@cs.sjcetpalai.ac.in>
2024-10-05 16:23:50 +0530

    Add files via upload

---
+++ arithmetis,comparison_and_logical_operaions.py
@@ -0,0 +1,14 @@
+'''
+AUTHOR:Alex roy
+Date: 05-10-2024
+Python program for arithmetics logical and comparison operators
+'''
+a=10
+b=5
+print(f"Sum:{a},Division:{b}")
+c=a>b
+print("Is a greater than b?:",c)
+d=a==b
+print("Are a and b equal?:",d)
+print("Logical AND:",a>b and a>b)
+print("Logical OR:",c or b)
