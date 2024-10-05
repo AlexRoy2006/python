0ed638d1893a9762ae4e5117fd3bcfe135f3e7da
AlexRoy2006 <alexroy2028@cs.sjcetpalai.ac.in>
2024-10-05 16:24:12 +0530

    Add files via upload

---
+++ twinkle.py
@@ -0,0 +1,6 @@
+'''
+AUTHOR:Alex roy
+Date: 05-10-2024
+Python program to get the 'tinkle twikle "in position
+'''
+print("Twinkle, twinkle, little star,\n\t\tHow I wonder what you are!\n\t\t\t\tUp above the world so high,\n\t\t\t\tLike a diamond in the sky.\nTwinkle, twinkle, little star,\n\t\tHow I wonder what you are")
