27d0cfc23d3171c3fc472b92ffc4afff460c646c
AlexRoy2006 <alexroy2028@cs.sjcetpalai.ac.in>
2024-10-05 16:22:37 +0530

    Add files via upload

---
+++ joshdetails.py
@@ -0,0 +1,9 @@
+'''
+AUTHOR:Alex roy
+Date: 05-10-2024
+Python program to get the  details of John
+'''
+name=input("Enter your name:")
+age=int(input("Enter your age:"))
+#print("Hello,",name,"! You are",age,"years old")
+print(f"Hello,{name}!you are {age} years old.")
