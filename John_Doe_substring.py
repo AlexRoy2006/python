212263996bde88874688a8c2a100131171b303dc
AlexRoy2006 <alexroy2028@cs.sjcetpalai.ac.in>
2024-10-08 14:58:18 +0530

    Add files via upload

---
+++ John_Doe_substring.py
@@ -0,0 +1,12 @@
+'''
+AUTHOR:Alex roy
+Date: 08-10-2024
+Python program to extract a word
+'''
+First_name="John"
+Second_name="Doe"
+Full_name=First_name+" "+Second_name
+print(Full_name)
+length=len(First_name)
+extract_First_name=Full_name[:length]
+print(extract_First_name)
