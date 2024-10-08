88ce97ee3facb156188d1f2a924c9de214690262
AlexRoy2006 <alexroy2028@cs.sjcetpalai.ac.in>
2024-10-08 14:58:44 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,18 @@
+'''
+AUTHOR:Alex roy
+Date: 08-10-2024
+Python program explaining the working of a simple calculator
+'''
+num1=int(input("Enter number1:"))
+num2=int(input("Enter number 2:"))
+num3=int(input("Enter a number 3"))
+sum=num1+num2
+print("The sum of num1 and num2 is:",sum)
+substract=num2-num1
+print("The difference when num1 is substracted from num1is:",substract)
+multiply=num1*num2
+print("The product of num1 and num2 is:",multiply)
+division=num1%num2
+print("The remainder when num1is divided by num2 is:")
+result=(num1+num2)*num3/2
+print("The result of (num1+num2)*num3/2 is:",result)
