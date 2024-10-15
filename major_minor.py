3ec3718c00b12f5ac9890e0250fe4fd920609f05
AlexRoy2006 <alexroy2028@cs.sjcetpalai.ac.in>
2024-10-15 14:24:18 +0530

    Add files via upload

---
+++ major_minor.py
@@ -0,0 +1,10 @@
+'''
+AUTHOR:Alex roy
+Date: 15-10-2024
+Python program to  check whether the person is major or minor
+'''
+age=int(input("Enter your age:"))
+if age>=18:
+    print("You are a major")
+else:
+    print("you are a minor")
