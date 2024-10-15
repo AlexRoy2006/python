ec529f5b76806e90fc704abd2b11305798800cb3
AlexRoy2006 <alexroy2028@cs.sjcetpalai.ac.in>
2024-10-15 14:56:35 +0530

    Add files via upload

---
+++ divisible_of_5.py
@@ -0,0 +1,10 @@
+'''
+AUTHOR:Alex roy
+Date: 15-10-2024
+Python program to know  whether the number is divisibe by 5
+'''
+num=int(input("Enter a number:"))
+if num %5==0:
+    print("Divisible by 5")
+else:
+    print("Not divisible by 5")
