eaa691f753dd073b9789b9f64902909d054ecfe6
AlexRoy2006 <alexroy2028@cs.sjcetpalai.ac.in>
2024-10-15 14:49:35 +0530

    Add files via upload

---
+++ driving_license_eligibility.py
@@ -0,0 +1,10 @@
+'''
+AUTHOR:Alex roy
+Date: 15-10-2024
+Python program to know if the person is eligible for driving license
+'''
+age=int(input("Enter your age:"))
+if age>=16:
+    print("Eligible for a driving license")
+else:
+    print("Not Eligible for driving license ")
