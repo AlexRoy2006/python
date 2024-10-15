a7cf9caa4f4ae1a961a3d8d50d772b3cfffe606b
AlexRoy2006 <alexroy2028@cs.sjcetpalai.ac.in>
2024-10-15 14:38:44 +0530

    Add files via upload

---
+++ compairing_x_and_y.py
@@ -0,0 +1,13 @@
+'''
+AUTHOR:Alex roy
+Date: 15-10-2024
+Python program to know which variable is greater
+'''
+num1=int(input("Enter number 1:",))
+num2=int(input("Enter number 2:"))
+if num1>num2:
+    print(num1,"is greater than ",num2)
+elif num1<num2:
+    print(num2,"is greter than",num1)
+else:
+    print(num1,"is equal to",num2)
