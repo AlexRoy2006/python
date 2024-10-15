12ed6359c41df89e6ef23685b183b685133175d2
AlexRoy2006 <alexroy2028@cs.sjcetpalai.ac.in>
2024-10-15 14:44:27 +0530

    Add files via upload

---
+++ greater_than 100.py
@@ -0,0 +1,8 @@
+'''
+AUTHOR:Alex roy
+Date: 15-10-2024
+Python program to know  if the number is greater than 100 
+'''
+num=int(input("Enter a Number:"))
+if num>100:
+    print("Number is greater than 100 ")
