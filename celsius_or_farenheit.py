7f2331c206a5cd16252a8c53eb9e548c3caa9b55
AlexRoy2006 <alexroy2028@cs.sjcetpalai.ac.in>
2024-10-22 15:11:37 +0530

    Update celsius_or_farenheit.py

--- celsius_or_farenheit.py
+++ celsius_or_farenheit.py
@@ -4,7 +4,7 @@ Date: 22-10-2024
 Python program to change Celsius into Farenheit and vice versa
 '''
 temp=float(input("Enter temperature:"))
-cf=input("Is this in (C)elsius or (F)arenheit")
+scale=input("Is this in (C)elsius or (F)arenheit")
 if cf=="C":
     f=(9/5*temp)+32
     print(temp,"Celsius is",f,"Farenheit.")
