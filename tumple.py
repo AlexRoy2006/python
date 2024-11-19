50084c25f28919520b9aae82a3b2c69fff34dcbf
AlexRoy2006 <alexroy2028@cs.sjcetpalai.ac.in>
2024-11-19 16:11:10 +0530

    Add files via upload

---
+++ tumple.py
@@ -0,0 +1,17 @@
+friuts=("apple","banana","strawberry","mongostene")
+print(len(friuts))
+print(friuts)
+print(friuts[-1])
+print(friuts[3])
+tuple1=(1,2,3)
+tuple2=(4,5,6)
+concatenated_tuple=tuple1+tuple2
+print(concatenated_tuple)
+tuple=(3,)*3
+print(tuple)
+colours=("red","green","blue","yellow","orange")
+print(colours[0:2])
+print(colours[-2:])
+student=("Alex",18,"CSE B")
+name,age,batch=student
+print(age) 
