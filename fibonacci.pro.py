6b7a1fb31d190387807fa93a219a1c2012170a66
AlexRoy2006 <alexroy2028@cs.sjcetpalai.ac.in>
2024-12-03 16:26:49 +0530

    Add files via upload

---
+++ fibonacci.pro.py
@@ -0,0 +1,9 @@
+def fibonacci_generator(n):
+    sequence=[]
+    first_no,second_no=0,1
+    for _ in range(n):
+        sequence.append(first_no)
+        first_no,second_no=second_no,first_no+second_no
+    return sequence
+print(fibonacci_generator(1000))
+
